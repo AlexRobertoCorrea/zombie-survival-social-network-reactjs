22ccf65ff80236cca7e4f8d2952210b3b068d751
AlexRobertoCorrea <alx.rcorrea@gmail.com>
2018-07-19 17:25:32 -0300

    webpack.config.js updated with css modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const ROOT_PATH = process.cwd();
 
 const config = {
+  mode: 'development',
   entry: `${ROOT_PATH}/client/App.js`,
   output: {
     path: `${ROOT_PATH}/.tmp`,
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    libraryTarget: 'umd'
   },
   watch: true,
   module: {
@@ -13,6 +15,22 @@ const config = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+              modules: true,
+              localIdentName: "[local]___[hash:base64:5]"
+            }
+          }
+        ]
       }
     ]
   }
