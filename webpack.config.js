360ea9727b4d07d787c2fa8d411a56c825fe0b43
AlexRobertoCorrea <alx.rcorrea@gmail.com>
2018-07-20 17:19:26 -0300

    webpack updated to recognize index.js as main file

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const ROOT_PATH = process.cwd();
 
 const config = {
   mode: 'development',
-  entry: `${ROOT_PATH}/client/App.js`,
+  entry: `${ROOT_PATH}/client/index.js`,
   output: {
     path: `${ROOT_PATH}/.tmp`,
     filename: 'bundle.js',
@@ -12,9 +12,9 @@ const config = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: 'babel-loader'
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
       },
       {
         test: /\.css$/,
