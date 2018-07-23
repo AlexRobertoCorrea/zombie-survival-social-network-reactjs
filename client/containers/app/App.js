0b5e81fcceed44d1da5e4a465a1a59a9ed05fa87
AlexRobertoCorrea <alx.rcorrea@gmail.com>
2018-07-23 14:46:52 -0300

    App.js updated with a header component

--- client/containers/app/App.js
+++ client/containers/app/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { inject, observer } from 'mobx-react';
+
+import Header from '../../components/header/header';
 // import btn from 'styles.css';
 
 const App = ({ people }) => {
   if (!people.fetchingData) {
     return <div>
+      <Header/>
       <pre>
         {JSON.stringify(people.toJSON().people, null, 2) };
         </pre>
