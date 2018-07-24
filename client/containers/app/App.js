d931d634de12157d61cc97282fc3d6150d351b7e
AlexRobertoCorrea <alx.rcorrea@gmail.com>
2018-07-23 23:09:39 -0300

    App.js uses ListPeople component

--- client/containers/app/App.js
+++ client/containers/app/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { inject, observer } from 'mobx-react';
+import ProgressBar from 'react-toolbox/lib/progress_bar';
 
-import Header from '../../components/header/header';
+import Header from '../../components/header/Header';
+import ListPeople from '../../components/list-people/List-people';
 // import btn from 'styles.css';
 
 const App = ({ people }) => {
   if (!people.fetchingData) {
     return <div>
       <Header/>
-      <pre>
-        {JSON.stringify(people.toJSON().people, null, 2) };
-        </pre>
+      <ListPeople people={people.toJSON().people}/>
     </div>;
   }
   
-  return <p>Aguarde ...</p>;
+  return <ProgressBar type='circular' mode='indeterminate' />;
 };
 
 export default inject('people')(observer(App));
